23297dc68d4f3b66296cc6c3196f8fb8d86c5d1b
yoojinche <chesunny917@gmail.com>
2025-11-04 16:16:23 +0900

    ✨feat: Member 엔티티 추가

---
+++ app/src/main/java/com/example/sookwalk/entity/Member.kt
@@ -0,0 +1,16 @@
+package com.example.sookwalk.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "members")
+data class Member(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val email: String,
+    val nickname: String,
+    val loginId: String,
+    val password: String,
+    var isNotice: Boolean,
+    var isTracked: Boolean,
+    var isDark: Boolean
+)
