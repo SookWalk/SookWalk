1149b32f5a5705d7da67e05edbdfd07654a0b8ed
임지영 <141422840+naneunimjee@users.noreply.github.com>
2025-11-22 03:21:23 +0900

    ✨ feat: RoomDB에 저장된 값으로 유저 정보 가져와서 띄우기

--- app/src/main/java/com/example/sookwalk/presentation/screens/member/MyPageScreen.kt
+++ app/src/main/java/com/example/sookwalk/presentation/screens/member/MyPageScreen.kt
@@ -1,6 +1,8 @@
 package com.example.sookwalk.presentation.screens.member
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,28 +25,49 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.sookwalk.R
+import com.example.sookwalk.data.local.entity.user.UserEntity
 import com.example.sookwalk.presentation.components.TopBar
+import com.example.sookwalk.presentation.viewmodel.UserViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun MyPageScreen(
-    // viewModel: TodoViewModel,
+    viewModel: UserViewModel,
     // navController: NavController,
     // backStackEntry: NavBackStackEntry
 ) {
+
+
+    val context = LocalContext.current
+
+    // --- 2. ViewModel의 StateFlow를 구독하여 Room DB의 사용자 정보를 실시간으로 받습니다. ---
+    val currentUser by viewModel.currentUser.collectAsState()
+
     Scaffold(
         topBar = {
             TopBar(
@@ -67,41 +90,51 @@ fun MyPageScreen(
             ) {
                 // 프로필, 사용자 정보
                 item {
-                    // 사용자 이미지 받아오는 로직 필요
-                    Image(
-                        // R.drawable.tode는 임시 파일 (깃에 추가 X)
-                        painter = painterResource(id = R.drawable.tode),
-                        contentDescription = "Profile Image",
+                    Box(
                         modifier = Modifier
-                            .fillMaxSize(0.7f)
-                            .aspectRatio(1f)    // 가로:세로 1:1
-                            .padding(12.dp)
-                            .clip(CircleShape),
-                        contentScale = ContentScale.Crop
-                    )
-
-                // 닉네임, 학과 정보
-                    Column(
-                        modifier = Modifier.padding(8.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                            .fillMaxWidth(0.7f)
+                            .aspectRatio(1f)
                     ) {
 
-                        Text(
-                            // 사용자 정보 가져오는 로직 나중에 필요
-                            "닉네임",
-                            style = MaterialTheme.typography.bodyLarge,
-                            fontWeight = FontWeight.Bold
-                        )
-
-                        Spacer(modifier = Modifier.height(4.dp))
-
-                        Text(
-                            // 사용자 학과 정보 가져오는 로직 나중에 필요
-                            "컴퓨터과학전공",
-                            style = MaterialTheme.typography.bodyMedium
+                        AsyncImage(
+                            model = ImageRequest.Builder(context)
+                                // Room DB에서 가져온 URL을 사용
+                                .data(
+                                    currentUser?.profileImageUrl ?: R.drawable.default_profile_image
+                                )
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Profile Image",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(12.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop,
+                            placeholder = painterResource(id = R.drawable.default_profile_image)
                         )
                     }
-                }
+
+                        // 닉네임, 학과 정보
+                        Column(
+                            modifier = Modifier.padding(8.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+
+                            Text(
+                                text = currentUser?.nickname ?: "정보 없음",
+                                style = MaterialTheme.typography.bodyLarge,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            Spacer(modifier = Modifier.height(4.dp))
+
+                            Text(
+                                // 사용자 학과 정보 가져오는 로직 나중에 필요
+                                text = currentUser?.major ?: "정보 없음",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
 
                 // 프로필 수정 버튼
                 item {
@@ -126,9 +159,10 @@ fun MyPageScreen(
 
                 // 계정 정보 란
                 item {
-                    Box(modifier = Modifier
-                        .fillMaxWidth()          // LazyColumn 아이템 전체 너비 차지
-                        .padding(horizontal = 12.dp) // 좌우 여백
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()          // LazyColumn 아이템 전체 너비 차지
+                            .padding(horizontal = 12.dp) // 좌우 여백
                     ) {
                         Text(
                             "계정 정보",
@@ -158,7 +192,7 @@ fun MyPageScreen(
                             Spacer(modifier = Modifier.width(4.dp))
 
                             // 아이디 정보 불러오는 로직 필요
-                            Text("noonsong", style = MaterialTheme.typography.labelSmall)
+                            Text(currentUser?.loginId?:"정보 없음", style = MaterialTheme.typography.labelSmall)
                         }
 
                         Row(
@@ -173,7 +207,10 @@ fun MyPageScreen(
                             Spacer(modifier = Modifier.width(4.dp))
 
                             // 이메일 정보 불러오는 로직 필요
-                            Text("noonsong@sookmyung.ac.kr", style = MaterialTheme.typography.labelSmall)
+                            Text(
+                                currentUser?.email?: "정보 없음",
+                                style = MaterialTheme.typography.labelSmall
+                            )
                         }
 
                         Row(
